9d418d7b96b43a24ec54469595dbc1eb505d22f2
Leo <geniusshiun@gmail.com>
2018-12-11 15:31:32 +0800

    Update

--- focus/special_focus.py
+++ focus/special_focus.py
@@ -38,7 +38,7 @@ def myCompare(a,b):
     except:
         pass
         return 0
-def createNewA(filepath,article,articleID,beautiful,stone):
+def createNewA(filepath,article,articleID,beautiful,stone,monkey,oldman,lu_1):
     """Write the new file A based on Levenshtein ratio
     
     Computer the highest Levenshtein ratio between different reference data.
@@ -57,10 +57,13 @@ def createNewA(filepath,article,articleID,beautiful,stone):
         pass
     """
     #load .cm files   
+    inwhichReference = {0:'null',1:'警廣',2:'美人恩',3:'石頭記',4:'西遊記',5:'老殘遊記',6:'魯迅吶喊'}
     number = int(re.findall('A(\d+)',filepath)[0])
     filename = re.findall('(A\d+)',filepath)[0]
     oridata = []
     inputlist = []
+    #if number > 10000:
+    #    return False
     with open(filepath,'r',encoding = 'utf8') as f:
         for line in f.readlines():
             
@@ -73,43 +76,71 @@ def createNewA(filepath,article,articleID,beautiful,stone):
             except: #Exception as e:
                 pass
     data = ''.join(inputlist)
+    if len(data) < 20:
+        print('ASR bad')
+        return number,'0','ASR length < 20'
     maxratio = 0
     referenceCandidata = ''
     if number > 10000:
         computerArticle = article
     else:
-        computerArticle = beautiful
+        computerArticle = []
+        computerArticle.extend(beautiful)
         computerArticle.extend(stone)
-    
-    print(number)
+        computerArticle.extend(monkey)
+        computerArticle.extend(oldman)
+        computerArticle.extend(lu_1)
     for reference in computerArticle:
         ratio = lev.ratio(data,reference)
         if ratio > maxratio:
             maxratio = ratio
             referenceCandidata = reference
+    referenceIndex = 0
     if maxratio > 0.3:
         if number > 10000:
-            print(maxratio,referenceCandidata[:10],articleID[referenceCandidata])
+            print(number,maxratio,referenceCandidata[:10],articleID[referenceCandidata])
+            referenceIndex = 1
         else:
-            print(maxratio,referenceCandidata[:10])
+            print(number,maxratio,referenceCandidata[:10])
+            if referenceCandidata in beautiful: 
+                referenceIndex = 2
+            elif referenceCandidata in stone: 
+                referenceIndex = 3
+            elif referenceCandidata in monkey: 
+                referenceIndex = 4
+            elif referenceCandidata in oldman: 
+                referenceIndex = 5
+            elif referenceCandidata in lu_1: 
+                referenceIndex = 6
+        
         #writ new A
-        with open(join('newA',filename+'.cm'),'w',encoding = 'utf8') as f:
+        with open(join('kaggle7',filename+'.cm'),'w',encoding = 'utf8') as f:
             for line in oridata:
                 f.write(line)
             f.write('1\t'+referenceCandidata+'\t0.99')
-    
-
+        return filename,maxratio,inwhichReference[referenceIndex]
+    return filename,maxratio,inwhichReference[referenceIndex]
+def loadreference(filepath):
+    data = []
+    with open(filepath,'r',encoding='utf8') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) > 20:
+                data.append(line)
+    return data
 def main():
     
     articleID = {}
     article = []
     beautiful = []
     stone = []
-    with open('美人恩.txt','r',encoding='utf8') as f:
-        for line in f.readlines():
-            line = line.strip()
-            if len(line) > 5:
-                beautiful.append(line)
+    monkey = []
+    oldman = []
+    lu_1 = []#吶喊
+    beautiful = loadreference('美人恩.txt')
+    monkey = loadreference('西遊記.txt')
+    oldman = loadreference('老殘遊記.txt')
+    lu_1 = loadreference('吶喊.txt')
     with open('石頭記.txt','r',encoding='utf8') as f:
         for line in f.readlines():
             line = line.strip().replace(' ','')
@@ -146,11 +177,23 @@ def main():
     
     input_text_path = sorted(glob.glob(join('A','*.cm')), key=functools.cmp_to_key(myCompare))
     pool = mp.Pool()
+    reslist = []
     for filepath in input_text_path:
-        args = [filepath,article,articleID,beautiful,stone]
+        args = [filepath,article,articleID,beautiful,stone,monkey,oldman,lu_1]
         res = pool.apply_async(func=createNewA, args=args)
+        reslist.append(res)
     pool.close()
     pool.join()
+    f = open('result.log','w',encoding='utf8')
+    for res in reslist:
+        #filename,maxratio,inwhichReference[referenceIndex]
+        result = res.get()
+        print(result)
+        filename = str(result[0])
+        maxratio = str(result[1])[:5]
+        comment = result[2]
+        f.write(filename+'\t'+maxratio+'\t'+comment+'\n')
+    f.close()
 
 if __name__ == '__main__':
     main()
